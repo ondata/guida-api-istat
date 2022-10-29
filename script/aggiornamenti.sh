26464f9867eb3c08891d23dfa47003a33c89e320
Andrea Borruso <aborruso@gmail.com>
2022-10-29 09:29:05 +0200

    close Attivare URL corretto in feed RSS #16

--- script/aggiornamenti.sh
+++ script/aggiornamenti.sh
@@ -11,6 +11,8 @@ mkdir -p "$folder"/rawdata
 mkdir -p "$folder"/../risorse
 
 URL="https://www.istat.it/js/rsssep.php"
+dataflow="http://sdmx.istat.it/SDMXWS/rest/dataflow/IT1"
+URLbase="http://dati.istat.it/Index.aspx?DataSetCode="
 
 # leggi la risposta HTTP del sito
 code=$(curl -s -kL -o /dev/null -w '%{http_code}' "$URL")
@@ -21,8 +23,25 @@ if [ $code -eq 200 ]; then
   cp "$folder"/rawdata/tmp_aggiornamenti.csv "$folder"/../risorse/aggiornamenti.csv
   dos2unix "$folder"/../risorse/aggiornamenti.csv
 
-  mlr --csv put '$guid="https://trigger.it/".$guid;$link=$guid' "$folder"/../risorse/aggiornamenti.csv >"$folder"/../risorse/tmp_rss.csv
+  # download the dataflows
+  curl -kL "$dataflow" >"$folder"/rawdata/tmp_dataflow.xml
 
+  # extract id and name from dataflows
+  <"$folder"/rawdata/tmp_dataflow.xml xq -c '."message:Structure"."message:Structures"."structure:Dataflows"."structure:Dataflow"[]|{id:."structure:Structure".Ref."@id",code:."@id"}' >"$folder"/rawdata/tmp_dataflow.jsonl
+
+  # convert jsonl to csv
+  mlr --j2c label id,guid "$folder"/rawdata/tmp_dataflow.jsonl >"$folder"/rawdata/tmp_dataflow.csv
+
+  # add the name of the dataset to SDMX info
+  mlr --csv join --ul -j guid -f "$folder"/../risorse/aggiornamenti.csv then unsparsify "$folder"/rawdata/tmp_dataflow.csv >"$folder"/rawdata/tmp_aggiornamenti.csv
+
+  # mv the tmp file to the final file
+  mv "$folder"/rawdata/tmp_aggiornamenti.csv "$folder"/../risorse/aggiornamenti.csv
+
+  # create the CSV source for ogr2ogr
+  mlr --csv put '$guid=$id;$guid="http://dati.istat.it/Index.aspx?DataSetCode=".$guid;$link=$guid' then cut -x -f id "$folder"/../risorse/aggiornamenti.csv >"$folder"/../risorse/tmp_rss.csv
+
+  # build the RSS
   ogr2ogr  -f geoRSS  -dsco TITLE="Aggiornamento dati ISTAT" -dsco LINK="https://ondata.github.io/guida-api-istat/risorse/sdmx.rss" -dsco DESCRIPTION="Un feed per sapere quando c'è un aggiornamento sulle basi ISTAT"  "$folder"/../risorse/sdmx.rss "$folder"/../risorse/tmp_rss.csv -oo AUTODETECT_TYPE=YES
 fi
 
