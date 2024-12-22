d670c47a001104c3cc80ca227f7eb37c29074ffa
aborruso <aborruso@gmail.com>
2024-12-22 13:17:06 +0100

    close #27

--- script/jiku_istat/jiku_istat.sh
+++ script/jiku_istat/jiku_istat.sh
@@ -27,6 +27,6 @@ mlr --ijsonl --ocsv --quote-all --headerless-csv-output cut -f itName then uniq 
 cp "$folder"/tmp/istatcats.csv "$folder"/../../data/jiku_istat/istatcats.csv
 
 # crea json dei dataflow, come richiesti da jiku
-mlr -I --jsonl --from "$folder"/tmp/istatcats.jsonl rename id,dataflowId then put '$enURL="https://esploradati.istat.it/RefMeta/template/GenericMetadataTemplate.html?nodeId=DW&metadataSetId=MDS_DIFF_REPORT&reportId=".$refId."&lang=en&BaseUrlMDA=https://esploradati.istat.it/METADATA_API"' then put '$itUrl="https://esploradati.istat.it/RefMeta/template/GenericMetadataTemplate.html?nodeId=DW&metadataSetId=MDS_DIFF_REPORT&reportId=".$refId."&lang=it&BaseUrlMDA=https://esploradati.istat.it/METADATA_API"'
+mlr -I --jsonl --from "$folder"/tmp/istatcats.jsonl rename id,dataflowId then put '$enURL="https://esploradati.istat.it/RefMeta/template/GenericMetadataTemplate.html?nodeId=DW&metadataSetId=MDS_DIFF_REPORT&reportId=".$refId."&lang=en&BaseUrlMDA=https://esploradati.istat.it/METADATA_API"' then put '$itUrl="https://esploradati.istat.it/RefMeta/template/GenericMetadataTemplate.html?nodeId=DW&metadataSetId=MDS_DIFF_REPORT&reportId=".$refId."&lang=it&BaseUrlMDA=https://esploradati.istat.it/METADATA_API"' then put '$getMetadataURL="https://esploradati.istat.it/METADATA_API/api/getMetadata?metadataSetId=MDS_DIFF_REPORT&reportId=".$refId'
 
-mlr --ijsonl --ojson cut -o -f dataflowId,refId,enName,itName,enURL,itUrl "$folder"/tmp/istatcats.jsonl >"$folder"/../../data/jiku_istat/istatcats.json
+mlr --ijsonl --ojson cut -o -f dataflowId,refId,enName,itName,enURL,itUrl,getMetadataURL "$folder"/tmp/istatcats.jsonl >"$folder"/../../data/jiku_istat/istatcats.json
